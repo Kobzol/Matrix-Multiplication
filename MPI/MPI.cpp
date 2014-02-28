e992d0ce2fc987e904bcec67e127d6dc86d80cac
Jakub Beránek <berykubik@gmail.com>
2014-02-28 22:24:36 +0100

    Initial commit.

---
+++ MPI/MPI.cpp
@@ -0,0 +1,30 @@
+﻿#include "mpi.h"
+#include <iostream>
+
+#include "matrix.h"
+
+int main(int argc, char argv[])
+{
+	MPI_Init(NULL, NULL);
+	
+	int rank;
+	int size;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (rank == 0)
+	{
+		
+	}
+	else
+	{
+		
+	}
+
+	
+	MPI_Finalize();
+
+	return 0;
+}
+
---
+++ MPI/matrix.h
@@ -0,0 +1,14 @@
+void setMatrices();
+
+void shuffleMatrices();
+void initialSendMatrices();
+void initialReceiveMatrices();
+
+void resendMatrices();
+void computeProduct();
+
+void returnMatrix();
+
+void composeMatrix();
+
+void printMatrix();
